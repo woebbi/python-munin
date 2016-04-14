8bf1ea6f40301598d95a5e35fe5d60396bc469ac
madman <madman.hust@gmail.com>
2016-04-14 23:42:01 +0700

    add more status mongo

--- munin/mongodb.py
+++ munin/mongodb.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python
 
 import os
+import sys
 from munin import MuninPlugin
 import pymongo
 
 
 class MuninMongoPlugin(MuninPlugin):
+    dbname_in_args = False
     category = "Mongo"
 
-    def __init__(self):
+    def __init__(self, config='serverStatus'):
         super(MuninMongoPlugin, self).__init__()
+        self.dbname = None
+        if self.dbname_in_args:
+            self.dbname = sys.argv[0].rsplit('_', 1)[-1]
+        if not self.dbname:
+            self.dbname = os.environ.get('MONGODB_DATABASE')
+
         if 'MONGO_DB_URI' in os.environ:
-            c = pymongo.MongoClient(os.environ['MONGO_DB_URI'])
+            self.connection = pymongo.MongoClient(os.environ['MONGO_DB_URI'])
         else:
-            c = pymongo.MongoClient()
-        self.server_status = c.admin.command('serverStatus', workingSet=True)
+            self.connection = pymongo.MongoClient()
+        self.server_status = self.connection.admin.command(config, workingSet=True)
+
+    @property
+    def db(self):
+        if not hasattr(self, '_db'):
+            self._db = getattr(self.connection, self.dbname)
+        return self._db
 
     def autoconf(self):
-        return bool(self.server_status)
+        return bool(self.connection)
