f265ecfc9607ed8dc2ee0c9ec6fc5bbebdd4efb1
madman <madman.hust@gmail.com>
2016-04-15 11:26:18 +0700

    update mongo

--- munin/mongodb.py
+++ munin/mongodb.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 
 import os
-import sys
 from munin import MuninPlugin
 import pymongo
 
 
 class MuninMongoPlugin(MuninPlugin):
-    dbname_in_args = False
+
     category = "Mongo"
+    dbname = None
 
     def __init__(self, config='serverStatus'):
         super(MuninMongoPlugin, self).__init__()
-        self.dbname = None
-        if self.dbname_in_args:
-            self.dbname = sys.argv[0].rsplit('_', 1)[-1]
         if not self.dbname:
             self.dbname = os.environ.get('MONGODB_DATABASE')
 
@@ -26,6 +23,8 @@ class MuninMongoPlugin(MuninPlugin):
 
     @property
     def db(self):
+        if self.dbname is None:
+            return None
         if not hasattr(self, '_db'):
             self._db = getattr(self.connection, self.dbname)
         return self._db
