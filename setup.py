695b6704549ba0a1568cae058c6652fef573d6af
madman <madman.hust@gmail.com>
2016-04-15 11:57:59 +0700

    update setup

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(
     name='munin',
     version=version,
     description='Framework for building Munin plugins',
-    author='Samuel Stauffer',
-    author_email='samuel@descolada.com',
-    url='http://github.com/samuel/python-munin/tree/master',
+    author='MADMAN Team',
+    author_email='madman.hust@mail.com',
+    url='https://github.com/madmanteam/python-munin/tree/master',
     packages=['munin'],
     classifiers=[
         'Intended Audience :: Developers',
